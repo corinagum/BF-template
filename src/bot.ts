bb8ee078cc35d2bfe28f2a283f154a718de3308a
Corina <14900841+corinagum@users.noreply.github.com>
2021-04-23 13:56:15 -0700

    Create EchoBot

---
+++ src/bot.ts
@@ -0,0 +1,29 @@
+import { ActivityHandler, MessageFactory, TurnContext } from 'botbuilder';
+
+export class EchoBot extends ActivityHandler {
+  constructor() {
+    super();
+
+    this.onMembersAdded(async (context: TurnContext, next) => {
+      const membersAdded = context.activity.membersAdded;
+
+      for (const member of membersAdded) {
+        if (member.id !== context.activity.recipient.id) {
+          const localeText = `\nThis activity's locale is ${context.activity.locale || 'not defined'}`;
+          const welcomeText = `Welcome, ${member.name}! This is a ${process.env.BOT_NAME}, an echo bot. ${localeText}`;
+          await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+        }
+      }
+
+      await next();
+    });
+
+    this.onMessage(async (context: TurnContext, next) => {
+      const replyText = `Echo: ~${context.activity.text}~`;
+
+      await context.sendActivity(MessageFactory.text(replyText, replyText));
+
+      await next();
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { BotFrameworkAdapter, InputHints, TurnContext } from 'botbuilder';
 import { config } from 'dotenv';
 import * as path from 'path';
 import * as restify from 'restify';
+import { EchoBot } from './bot';
 
 const ENV_FILE = path.join(__dirname, '..', '.env');
 config({ path: ENV_FILE });
@@ -9,8 +10,9 @@ config({ path: ENV_FILE });
 /* HTTP Server */
 const server = restify.createServer();
 server.name = process.env.BOT_NAME;
+const botName = server.name;
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-  console.log(`\n ${server.name} server listening to ${server.url}`);
+  console.log(`\n ${botName} server listening to ${server.url}`);
 });
 
 /* BotFrameworkAdapter: https://aka.ms/about-bot-adapter */
@@ -21,7 +23,7 @@ const adapter = new BotFrameworkAdapter({
 
 /* Error handling */
 const onTurnErrorHandler = async (context: TurnContext, error: Error) => {
-  console.error(`\n ${server.name}: [onTurnError] unhandled error: ${error}`);
+  console.error(`\n ${botName}: [onTurnError] unhandled error: ${error}`);
 
   /* Send trace activity to display errors in Emulator (locally running bot)
     https://docs.microsoft.com/en-us/azure/bot-service/using-trace-activities?view=azure-bot-service-4.0&tabs=javascript
@@ -30,12 +32,20 @@ const onTurnErrorHandler = async (context: TurnContext, error: Error) => {
     'OnTurnError Trace',
     `${error}`,
     'https://www.botframework.com/schemas/error',
-    `${process.env.BOT_NAME} TurnError`
+    `${botName} TurnError`
   );
 
-  const onTurnErrorMessage = `${process.env.BOT_NAME} encountered an error. Please fix the bot source code.`;
+  const onTurnErrorMessage = `${botName} encountered an error. Please fix the bot source code.`;
 
   await context.sendActivity(onTurnErrorMessage, onTurnErrorMessage, InputHints.IgnoringInput);
 };
 
 adapter.onTurnError = onTurnErrorHandler;
+
+const echoBot = new EchoBot();
+
+server.post('/api/messages', (req, res) => {
+  adapter.processActivity(req, res, async context => {
+    await echoBot.run(context);
+  });
+});
