62f9d0b1bf73200db9e87e50fd9ac905ee49aa80
Corina <14900841+corinagum@users.noreply.github.com>
2021-04-23 12:10:59 -0700

    Add error handling

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,41 @@
+import { BotFrameworkAdapter, InputHints, TurnContext } from 'botbuilder';
 import { config } from 'dotenv';
 import * as path from 'path';
 import * as restify from 'restify';
 
-
 const ENV_FILE = path.join(__dirname, '..', '.env');
 config({ path: ENV_FILE });
 
-// import { BotFrameworkAdapter } from 'botbuilder';
-
-//
-
+/* HTTP Server */
 const server = restify.createServer();
-server.name = process.env.SERVER_NAME
+server.name = process.env.BOT_NAME;
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-  console.log(`\n${server.name} listening to ${server.url}`);
-})
+  console.log(`\n ${server.name} server listening to ${server.url}`);
+});
+
+/* BotFrameworkAdapter: https://aka.ms/about-bot-adapter */
+const adapter = new BotFrameworkAdapter({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+/* Error handling */
+const onTurnErrorHandler = async (context: TurnContext, error: Error) => {
+  console.error(`\n ${server.name}: [onTurnError] unhandled error: ${error}`);
+
+  /* Send trace activity to display errors in Emulator (locally running bot)
+    https://docs.microsoft.com/en-us/azure/bot-service/using-trace-activities?view=azure-bot-service-4.0&tabs=javascript
+  */
+  await context.sendTraceActivity(
+    'OnTurnError Trace',
+    `${error}`,
+    'https://www.botframework.com/schemas/error',
+    `${process.env.BOT_NAME} TurnError`
+  );
+
+  const onTurnErrorMessage = `${process.env.BOT_NAME} encountered an error. Please fix the bot source code.`;
+
+  await context.sendActivity(onTurnErrorMessage, onTurnErrorMessage, InputHints.IgnoringInput);
+};
+
+adapter.onTurnError = onTurnErrorHandler;
