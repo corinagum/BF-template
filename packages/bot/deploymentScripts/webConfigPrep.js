510d1aceabd40869e2db1c4b66e556f2f67bb288
Corina <14900841+corinagum@users.noreply.github.com>
2021-04-28 14:00:38 -0700

    Clean up scripts

--- packages/bot/deploymentScripts/webConfigPrep.js
+++ packages/bot/deploymentScripts/webConfigPrep.js
@@ -11,11 +11,11 @@ const replace = require('replace');
 const WEB_CONFIG_FILE = './web.config';
 
 if (fs.existsSync(path.resolve(WEB_CONFIG_FILE))) {
-    replace({
-        regex: "url=\"index.js\"",
-        replacement: "url=\"lib/index.js\"",
-        paths: ['./web.config'],
-        recursive: false,
-        silent: true,
-    })
+  replace({
+    regex: 'url="index.js"',
+    replacement: 'url="lib/index.js"',
+    paths: ['./web.config'],
+    recursive: false,
+    silent: true
+  });
 }
