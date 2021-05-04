364f60b6ff10e9a1522c4f1c085f1dbf474eb949
Corina <14900841+corinagum@users.noreply.github.com>
2021-05-04 15:26:57 -0700

    DL POST

---
+++ packages/token-server/src/api/directLine/post.ts
@@ -0,0 +1,27 @@
+import { Server } from 'restify';
+import generateServiceToken from '../../utils/generateServiceToken';
+import renewDirectLineToken from '../../utils/renewServiceToken';
+
+export default function POSTTokenDL(server: Server): void {
+  server.post('/api/token/directline', async (req, res) => {
+    try {
+      // Need trusted origin handling
+      // const origin = req.header('origin');
+
+      const { token } = req.query;
+
+      try {
+        const result = await (token ? renewDirectLineToken(token) : generateServiceToken());
+
+        res.sendRaw(JSON.stringify(result, null, 2), {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        });
+      } catch (err) {
+        res.send(500, err.message, { 'Access-Control-Allow-Origin': '*' });
+      }
+    } catch (err) {
+      res.send(500, { message: err.message, stack: err.stack }, { 'Access-Control-Allow-Origin': '*' });
+    }
+  });
+}
